fd44cca271569881883f1c5685bcd8990146ab3a
RuslanKralin <kralin.front@gmail.com>
2024-04-27 23:13:43 +0300

    Added my project

---
+++ theme.ts
@@ -0,0 +1,20 @@
+"use client";
+
+import { createTheme } from "@mantine/core";
+
+export const theme = createTheme({
+  colors: {
+    purple: [
+        "#f5edff",
+        "#e5d7f7",
+        "#c8aded",
+        "#a97fe3",
+        "#9058da",
+        "#7f40d5",
+        "#7734d3",
+        "#6526bc",
+        "#5a21a8",
+        "#4d1b95"
+      ]
+  }
+});
